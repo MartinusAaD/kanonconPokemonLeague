ce9c8e36ab4be1cc3e72694decf1abc7bc2c3905
maraad97@gmail.com <maraad97@gmail.com>
2026-05-07 11:18:45 +0200

    feature: Page not scrolls/snaps to the top on new route, and remember position of previous page if you want to go back

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.module.css";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigationType } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const { pathname } = useLocation();
+  const navType = useNavigationType();
+
+  useEffect(() => {
+    if (navType !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, navType]);
+
   return (
     <>
       <header>
